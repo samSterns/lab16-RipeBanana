f972e7a39a3ad060754d721338c6c0ddc5000ddb
samSterns <sam.g.sterns@gmail.com>
2020-01-09 11:07:22 -0800

    all test passing AND delete condition functionality done

--- __tests__/reviewer.test.js
+++ __tests__/reviewer.test.js
@@ -1,4 +1,7 @@
-const { getReviewer, getReviewers } = require('../lib/helpers/data-helpers');
+require('dotenv').config();
+
+const { getReviewer, getReviewers, getReview } = require('../lib/helpers/data-helpers');
+const Review = require('../lib/models/Review');
 
 const request = require('supertest');
 const app = require('../lib/app.js');
@@ -60,12 +63,27 @@ describe('reviewer routes', () => {
       });
   });
 
-  it('deletes a reviewer by id', async() => {
-    const reviewer = await getReviewer(); 
+  it('deletes a reviewer because they have No reviews', async() => {
+    const reviewWithNoReviews = await getReviewer();
+    await Review.deleteMany({ reviewer: reviewWithNoReviews._id });
+
     return request(app)
-      .delete(`/api/v1/reviewers/${reviewer._id}`)
+      .delete(`/api/v1/reviewers/${reviewWithNoReviews._id}`)
       .then(res => {
-        expect(res.body).toEqual(reviewer);
+        expect(res.body).toEqual(reviewWithNoReviews);
+      });
+  });
+
+  it('does Not delete a reviewer because they have written a review', async() => {
+    const review = await getReview(); 
+    const reviewerWithReview = await getReviewer({ _id: review.reviewer });
+    return request(app)
+      .delete(`/api/v1/reviewers/${reviewerWithReview._id}`)
+      .then(res => {
+        expect(res.body).toEqual({ 
+          message: 'This reviewer currently has reviews and cannot be deleted', 
+          status: 500
+        });
       });
   }); 
 
